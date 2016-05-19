d24370b5b70be3c36a8b8bdd4f585b2d0465d0c7
Philippe Gouin <bering@ringlogic.com>
2016-05-19 08:29:14 -0400

    You decide if collider on the controllers are triggers or not

--- Assets/F4VR/Scripts/F4VR_Controller.cs
+++ Assets/F4VR/Scripts/F4VR_Controller.cs
@@ -4,6 +4,9 @@
 public class F4VR_Controller : MonoBehaviour {
 
 
+	public bool isTrigger;
+
+
 	void Start()
 	{
 		SetupRigidbody ();
@@ -30,7 +33,7 @@ public class F4VR_Controller : MonoBehaviour {
 			col = gameObject.AddComponent<CapsuleCollider> ();
 		}
 
-		col.isTrigger = true;
+		col.isTrigger = isTrigger;
 		col.center = new Vector3 (0, -0.04f, 0);
 		col.radius = 0.05f;
 		col.height = 0.01f;
